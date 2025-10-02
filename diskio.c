b5cb28c6abcce3a4a675f0e72d9444c6e5238e27
Potassium-chromate <112847018+Potassium-chromate@users.noreply.github.com>
2025-10-02 21:37:02 +0800

    feat(fatfs): integrate FatFs with sd_spi driver
    - implement diskio.c to bridge FatFs and the custom sd_spi driver
    - basic disk_read and disk_write now interact with the kernel module
    - prepare user.c for FatFs usage (mounting, file operations)
    
    TODO:
    - complete FatFs integration in user.c for file read/write tests

---
+++ diskio.c
@@ -0,0 +1,152 @@
+/*-----------------------------------------------------------------------*/
+/* Low level disk I/O module SKELETON for FatFs     (C)ChaN, 2025        */
+/*-----------------------------------------------------------------------*/
+/* If a working storage control module is available, it should be        */
+/* attached to the FatFs via a glue function rather than modifying it.   */
+/* This is an example of glue functions to attach various exsisting      */
+/* storage control modules to the FatFs module with a defined API.       */
+/*-----------------------------------------------------------------------*/
+
+#include "ff.h"			/* Basic definitions of FatFs */
+#include "diskio.h"		/* Declarations FatFs MAI */
+#include "spi.h"
+
+/* Example: Declarations of the platform and disk functions in the project */
+//#include "platform.h"
+//#include "storage.h"
+
+/* Example: Mapping of physical drive number for each drive */
+#define DEV_FLASH	0	/* Map FTL to physical drive 0 */
+#define DEV_MMC		1	/* Map MMC/SD card to physical drive 1 */
+#define DEV_USB		2	/* Map USB MSD to physical drive 2 */
+
+
+/*-----------------------------------------------------------------------*/
+/* Get Drive Status                                                      */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+)
+{
+	if (pdrv != 0) return STA_NOINIT;
+    return 0;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Inidialize a Drive                                                    */
+/*-----------------------------------------------------------------------*/
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+	if (pdrv != 0) return STA_NOINIT;
+    return sd_init() ? 0 : STA_NOINIT;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	LBA_t sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+	if (pdrv != 0) return RES_PARERR;
+
+    for (UINT i = 0; i < count; i++) {
+        if (!sd_read_block(sector + i, buff + i * 512)) {
+            return RES_ERROR;
+        }
+    }
+    return RES_OK;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+#if FF_FS_READONLY == 0
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	LBA_t sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{	
+	if (pdrv != 0) return RES_PARERR;
+	
+    for (UINT i = 0; i < count; i++) {
+        if (!sd_write_block(sector + i, buff + i * 512)) {
+            return RES_ERROR;
+        }
+    }
+    return RES_OK;
+}
+
+#endif
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	/*
+	DRESULT res;
+	int result;
+
+	switch (pdrv) {
+	case DEV_RAM :
+
+		// Process of the command for the RAM drive
+
+		return res;
+
+	case DEV_MMC :
+
+		// Process of the command for the MMC/SD card
+
+		return res;
+
+	case DEV_USB :
+
+		// Process of the command the USB drive
+
+		return res;
+	}
+
+	return RES_PARERR;
+	*/
+	if (pdrv != 0) return RES_PARERR;
+
+    switch (cmd) {
+    case CTRL_SYNC:
+        return RES_OK;
+    case GET_SECTOR_SIZE:
+        *(WORD*)buff = 512;
+        return RES_OK;
+    case GET_BLOCK_SIZE:
+        *(DWORD*)buff = 1;
+        return RES_OK;
+    default:
+        return RES_PARERR;
+    }
+}
+
